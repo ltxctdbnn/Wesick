7912e61dbb5a3df2d4dab24e5a63ad7e642727ea
Park Jeong Hwan <jeonghwan.dev@gmail.com>
2021-04-12 12:14:52 +0900

    Add bootstrap

--- client/src/routes/SignIn.js
+++ client/src/routes/SignIn.js
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
-import axios from 'axios';
-import 'routes/css/SignIn.css';
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+// import "routes/css/SignIn.css";
+// import { Grid } from "@material-ui/core";
+import { Container, Row, Col } from "react-bootstrap";
 
 axios.defaults.withCredentials = true;
 
@@ -9,8 +11,8 @@ axios.defaults.withCredentials = true;
 function SignIn() {
   const url = `http://localhost:5000`;
   const history = useHistory();
-  const [userEmail, setUserEmail] = useState('');
-  const [userPassword, setUserPassword] = useState('');
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassword, setUserPassword] = useState("");
 
   // (로그인 폼) 입력 핸들러
   const onChangeHandler = (event) => {
@@ -18,9 +20,9 @@ function SignIn() {
       target: { name, value },
     } = event;
 
-    if (name === 'userEmail') {
+    if (name === "userEmail") {
       setUserEmail(value);
-    } else if (name === 'userPassword') {
+    } else if (name === "userPassword") {
       setUserPassword(value);
     }
   };
@@ -29,8 +31,8 @@ function SignIn() {
   const onSignInHandler = async (event) => {
     event.preventDefault();
     await axios
-      .post(url + '/', {
-        method: 'POST',
+      .post(url + "/", {
+        method: "POST",
         body: JSON.stringify({
           userEmail: userEmail,
           userPassword: userPassword,
@@ -39,39 +41,41 @@ function SignIn() {
       })
       .then((response) => {
         if (response.data.status === 400) {
-          alert('로그인 성공');
-          sessionStorage.setItem('accessToken', response.data.access_token);
-          sessionStorage.setItem('refreshToken', response.data.refresh_token);
-          sessionStorage.setItem('userid', JSON.stringify(response.data.user_object.id));
-          sessionStorage.setItem('nickname', JSON.stringify(response.data.user_object.nickname));
-          sessionStorage.setItem('email', JSON.stringify(response.data.user_object.email));
-          sessionStorage.setItem('usertype', JSON.stringify(response.data.user_object.usertype));
+          alert("로그인 성공");
+          sessionStorage.setItem("accessToken", response.data.access_token);
+          sessionStorage.setItem("refreshToken", response.data.refresh_token);
+          sessionStorage.setItem(
+            "userid",
+            JSON.stringify(response.data.user_object.id)
+          );
+          sessionStorage.setItem(
+            "nickname",
+            JSON.stringify(response.data.user_object.nickname)
+          );
+          sessionStorage.setItem(
+            "email",
+            JSON.stringify(response.data.user_object.email)
+          );
+          sessionStorage.setItem(
+            "usertype",
+            JSON.stringify(response.data.user_object.usertype)
+          );
 
-          window.location.replace('/');
+          window.location.replace("/");
         } else if (response.data.status === 401) {
-          alert('가입하지 않은 아이디이거나, 잘못된 비밀번호입니다.');
+          alert("가입하지 않은 아이디이거나, 잘못된 비밀번호입니다.");
         } else {
-          alert('error');
+          alert("error");
         }
       })
       .catch(() => {
-        alert('error for some reason');
+        alert("error for some reason");
       });
   };
 
-
   return (
-    <div
-      className="sign-container"
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '100%',
-        height: '100vh',
-      }}
-    >
-      <form style={{ display: 'flex', flexDirection: 'column' }}>
+    <div className="sign-container">
+      {/* <form style={{ display: "flex", flexDirection: "column" }}>
         <h2>토닥토닥</h2>
         <input
           name="userEmail"
@@ -95,7 +99,7 @@ function SignIn() {
             id="signup-btn"
             onClick={() => {
               history.push({
-                pathname: '/sign-up',
+                pathname: "/sign-up",
               });
             }}
           >
@@ -103,7 +107,53 @@ function SignIn() {
           </button>
           <button onClick={onSignInHandler}>로그인</button>
         </div>
-      </form>
+      </form> */}
+      <Container>
+        <Row>
+          <Col>로그인</Col>
+        </Row>
+        <Row>
+          <Col>
+            <input
+              name="userEmail"
+              type="email"
+              value={userEmail}
+              onChange={onChangeHandler}
+              placeholder="이메일"
+              required
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <input
+              name="userPassword"
+              type="password"
+              value={userPassword}
+              onChange={onChangeHandler}
+              placeholder="비밀번호"
+              required
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <button
+              id="signup-btn"
+              onClick={() => {
+                history.push({
+                  pathname: "/sign-up",
+                });
+              }}
+            >
+              회원가입
+            </button>
+          </Col>
+          <Col>
+            <button onClick={onSignInHandler}>로그인</button>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
